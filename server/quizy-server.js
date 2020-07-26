287f0031d9136cf1683568ea914df68b8134a257
Richy Henle <richy.henle@softgarden.de>
2020-07-26 22:58:05 +0200

    Fixed server

--- server/quizy-server.js
+++ server/quizy-server.js
@@ -42,7 +42,7 @@ app.get('/:gameId', function (req, res) {
         return;
     }
     var time = games[gameId].startTime;
-    if (time < Date.now() + 50000) {
+    if (time < Date.now() - 100000) {
         time = 0;
     }
     res.send({
